f23280228df6ad68beb9929bf5f86975c23b542f
Patrick Sheriddan <sheridp2@gmail.com>
2019-04-17 22:46:11 -0700

    able to get user latitude nad longitude render to component

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const SeasonDisplay = () => {
+  return <div>Seasons Display</div>;
+};
--- src/index.js
+++ src/index.js
@@ -1,30 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
-import CommentDetail from "./CommentDetail";
-const App = () => {
-  return (
-    <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45pm"
-        comment="Nice post"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 2:00AM"
-        comment="Very cool"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Mike"
-        timeAgo="Yesterday at 5;55pm"
-        comment="WOOOOOOOOOOOOOO"
-        avatar={faker.image.avatar()}
-      />
-    </div>
-  );
-};
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude })
+      },
+      err => console.log(err)
+    )
+  }
+
+  render(){
+    return <div>Latitude: {this.state.lat}</div>
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
